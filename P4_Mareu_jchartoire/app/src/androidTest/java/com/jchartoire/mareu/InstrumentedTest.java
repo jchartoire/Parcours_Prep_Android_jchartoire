9403e37a4dffc3f1f1e5e34ef4fc878916e2f973
jchartoire <>
2020-04-28 20:52:03 +0200

    Tests d'intégration terminés

--- P4_Mareu_jchartoire/app/src/androidTest/java/com/jchartoire/mareu/InstrumentedTest.java
+++ P4_Mareu_jchartoire/app/src/androidTest/java/com/jchartoire/mareu/InstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.jchartoire.mareu;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -8,8 +9,6 @@ import android.widget.DatePicker;
 
 import com.jchartoire.mareu.di.DI;
 import com.jchartoire.mareu.model.Meeting;
-import com.jchartoire.mareu.model.Room;
-import com.jchartoire.mareu.model.User;
 import com.jchartoire.mareu.service.ApiService;
 import com.jchartoire.mareu.service.DummyGenerator;
 
@@ -30,17 +29,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import static androidx.constraintlayout.widget.Constraints.TAG;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
@@ -53,8 +53,6 @@ import static com.jchartoire.mareu.tools.DateUtils.dateFormatter;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertEquals;
 
@@ -70,8 +68,6 @@ public class InstrumentedTest {
     private ItemListActivity itemListActivity;
     private ApiService apiService;
     private List<Meeting> dummyMeetings = DummyGenerator.dummyMeetings;
-    private List<User> dummyUsers = DummyGenerator.dummyUsers;
-    private List<Room> dummyRooms = DummyGenerator.dummyRooms;
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
@@ -92,11 +88,18 @@ public class InstrumentedTest {
         };
     }
 
+    public void resetDummyList() {
+        apiService.getMeetings().clear();
+        apiService.getMeetings().addAll(dummyMeetings);
+    }
+
     @Before
     public void setUp() {
         itemListActivity = activityTestRule.getActivity();
         assertThat(itemListActivity, notNullValue());
         apiService = DI.getApiService();
+        // restore dummy generated list in case of previous test modifications
+        resetDummyList();
     }
 
     @Test
@@ -112,7 +115,7 @@ public class InstrumentedTest {
      */
     @Test
     public void recyclerView_shouldNotBeEmpty() {
-        String dummyTitle = dummyMeetings.get(0).getTitle();
+        String dummyTitle = apiService.getMeetings().get(0).getTitle();
         // check RecyclerView is not empty
         onView(withId(R.id.recycler_view)).check(matches(hasMinimumChildCount(1)));
         // check item is not void and contains meeting title
@@ -128,12 +131,12 @@ public class InstrumentedTest {
     public void meetingsList_deleteAction_shouldRemoveItem() {
         // count meetings elements
         int meetingCount = apiService.getMeetings().size();
-        RecyclerView recyclerView = activityTestRule.getActivity().findViewById(R.id.recycler_view);
         // compare meetings count and recyclerView item count
+        RecyclerView recyclerView = activityTestRule.getActivity().findViewById(R.id.recycler_view);
         assertEquals(meetingCount, Objects.requireNonNull(recyclerView.getAdapter()).getItemCount());
         // click on delete button of the first item
         onView(allOf(withId(R.id.iv_delete_button), withParent(allOf(withId(R.id.item),
-                childAtPosition(withId(R.id.recycler_view), 1))), isDisplayed())).perform(click());
+                childAtPosition(withId(R.id.recycler_view), 0))), isDisplayed())).perform(click());
         // check that item list contains one item less
         assertEquals(meetingCount - 1, Objects.requireNonNull(recyclerView.getAdapter()).getItemCount());
     }
@@ -143,7 +146,7 @@ public class InstrumentedTest {
      */
     @Test
     public void meetingsList_clickItem_shouldLaunchDetail_andShowNeighboursName() {
-        // we store the name of the meeting at first position of the list
+        // store the name of the meeting at first position of the list
         Meeting meeting = apiService.getMeetings().get(0);
         String meetingName = meeting.getTitle();
         // perform a click on first item
@@ -158,26 +161,29 @@ public class InstrumentedTest {
      */
     @Test
     public void detailActivity_newMeetingEditor() {
-
         Calendar calendar = Calendar.getInstance();
         calendar.getTimeInMillis();
+        // fill up required fields
         onView(withId(R.id.add_fab)).perform(click());
-        onView(withId(R.id.et_meeting_title)).perform(ViewActions.typeText("Meeting Test"), closeSoftKeyboard());
+        onView(withId(R.id.et_meeting_title)).perform(ViewActions.typeText("New Meeting Test"), closeSoftKeyboard());
         onView(withId(R.id.actv_leader)).perform(ViewActions.typeText("adam.cook@lamzone.com"), closeSoftKeyboard());
         onView(withId(R.id.mactv_Participants)).perform(ViewActions.typeText("amy.hall@lamzone.com"), closeSoftKeyboard());
-        // Set a date on the date picker dialog
+        // Set a date on the date picker dialog (same as first dummyMeeting, today, 8:00, room 1)
         onView(withId(R.id.tv_clickable_date)).perform(click());
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH)));
         onView(withText("OK")).perform(click());
+        // try to save the new meeting
         onView(withId(R.id.ok_settings)).perform(click());
-        onView(withText(R.string.meeting_already_exist)).check(matches(isDisplayed()));
         // Meeting exist so error dialog show up. We click OK
+        onView(withText(R.string.meeting_already_exist)).check(matches(isDisplayed()));
         onView(withText("OK")).perform(click());
+        // change the room to room 10 (room 5 to 10 aren't used in dummyGenerator)
         onView(withId(R.id.spn_room)).perform(click());
         onData(anything()).atPosition(9).perform(click());
         onView(withId(R.id.ok_settings)).perform(click());
-        onView(withId(R.id.recycler_view)).check(matches(hasDescendant(withText(containsString("Meeting Test")))));
+        // check that new meeting is showing up in meetings list
+        onView(withId(R.id.recycler_view)).check(matches(hasDescendant(withText(containsString("New Meeting Test")))));
     }
 
     /**
@@ -185,38 +191,27 @@ public class InstrumentedTest {
      */
     @Test
     public void roomFilter_show_only_matching_room() {
+        // count the number of meetings registered in room 1
         int itemCount = 0;
-        for (int i = 0; i < dummyMeetings.size(); i++) {
-            if (dummyMeetings.get(i).getRoom().toString().equals("Salle 1")) {
+        for (int i = 0; i < apiService.getMeetings().size(); i++) {
+            if (apiService.getMeetings().get(i).getRoom().toString().equals("Salle 1")) {
                 itemCount++;
             }
         }
-        RecyclerView recyclerView = activityTestRule.getActivity().findViewById(R.id.recycler_view);
         onView(withId(R.id.recycler_view)).check(matches(hasMinimumChildCount(1)));
-
         // set filter by Room 1
         openActionBarOverflowOrOptionsMenu(itemListActivity.getBaseContext());
         onView(withText(R.string.filter_room_item)).perform(click());
         onView(withText("OK")).perform(click());
-        onView(withId(R.id.recycler_view)).check(matches(hasDescendant(withText(containsString("Réunion")))));
         // assert that there is only room matching meetings in list
+        RecyclerView recyclerView = activityTestRule.getActivity().findViewById(R.id.recycler_view);
         assertEquals(itemCount, Objects.requireNonNull(recyclerView.getAdapter()).getItemCount());
-
         // set filter by Room 10
         openActionBarOverflowOrOptionsMenu(itemListActivity.getBaseContext());
         onView(withText(R.string.filter_room_item)).perform(click());
         onView(allOf(childAtPosition(childAtPosition(withId(android.R.id.custom), 0), 0), isDisplayed())).perform(click());
-//TODO: Caused by: java.lang.RuntimeException: No data found matching: (is an instance of java.lang.String and a string containing "Salle 5")
-// contained values: <[Data: Salle 1 (class: com.jchartoire.mareu.model.Room) token: 0, Data: Salle 2 (class: com.jchartoire.mareu.model.Room)
-// token: 1, Data: Salle 3 (class: com.jchartoire.mareu.model.Room) token: 2, Data: Salle 4 (class: com.jchartoire.mareu.model.Room) token: 3,
-// Data: Salle 5 (class: com.jchartoire.mareu.model.Room) token: 4, Data: Salle 6 (class: com.jchartoire.mareu.model.Room) token: 5, Data: Salle 7
-// (class: com.jchartoire.mareu.model.Room) token: 6, Data: Salle 8 (class: com.jchartoire.mareu.model.Room) token: 7, Data: Salle 9 (class: com
-// .jchartoire.mareu.model.Room) token: 8, Data: Salle 10 (class: com.jchartoire.mareu.model.Room) token: 9]>
-        onData(allOf(is(instanceOf(String.class)),
-                containsString("Salle 5")))
-                .inRoot(isPlatformPopup())
-                .perform(click());
-
+        // select 9th item (room 10)
+        onData(anything()).inRoot(RootMatchers.isPlatformPopup()).atPosition(9).perform(click());
         onView(withText("OK")).perform(click());
         // assert that there is no room matching in list
         assertEquals(0, Objects.requireNonNull(recyclerView.getAdapter()).getItemCount());
@@ -230,13 +225,12 @@ public class InstrumentedTest {
         Calendar calendar = Calendar.getInstance();
         calendar.getTimeInMillis();
         String currentDate = dateFormatter.format(calendar.getTimeInMillis());
-        int countItem = 0;
-        for (int i = 0; i < dummyMeetings.size(); i++) {
-            if (dateFormatter.format(dummyMeetings.get(i).getStartDate()).equals(currentDate)) {
-                countItem++;
+        int itemCount = 0;
+        for (int i = 0; i < apiService.getMeetings().size(); i++) {
+            if (dateFormatter.format(apiService.getMeetings().get(i).getStartDate()).equals(currentDate)) {
+                itemCount++;
             }
         }
-        RecyclerView recyclerView = activityTestRule.getActivity().findViewById(R.id.recycler_view);
         onView(withId(R.id.recycler_view)).check(matches(hasMinimumChildCount(1)));
         // set filter by Date
         openActionBarOverflowOrOptionsMenu(itemListActivity.getBaseContext());
@@ -246,14 +240,15 @@ public class InstrumentedTest {
                 calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH)));
         onView(withText("OK")).perform(click());
         // assert that there is only current date matching in list
-        assertEquals(countItem, Objects.requireNonNull(recyclerView.getAdapter()).getItemCount());
+        RecyclerView recyclerView = activityTestRule.getActivity().findViewById(R.id.recycler_view);
+        assertEquals(itemCount, Objects.requireNonNull(recyclerView.getAdapter()).getItemCount());
     }
 
     /**
      * When filtering is active, check if new meeting is showing up in filtered list
      */
     @Test
-    public void FilteredList_shouldBeUpdated() {
+    public void filteredList_shouldBeUpdated() {
         Calendar calendar = Calendar.getInstance();
         calendar.getTimeInMillis();
         calendar.add(Calendar.MONTH, 1);
