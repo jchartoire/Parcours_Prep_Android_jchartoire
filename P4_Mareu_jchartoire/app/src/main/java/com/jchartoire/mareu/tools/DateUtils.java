8e47ae7c8869dccc1ff1a8fea607e9a9cb08acab
jchartoire <>
2020-04-29 08:59:54 +0200

    Correction SimpleDateFormat pattern (yyyy instead of YYYY)

--- P4_Mareu_jchartoire/app/src/main/java/com/jchartoire/mareu/tools/DateUtils.java
+++ P4_Mareu_jchartoire/app/src/main/java/com/jchartoire/mareu/tools/DateUtils.java
@@ -9,7 +9,7 @@ public final class DateUtils {
     private DateUtils() {
     }
 
-    public static SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/YYYY", Locale.FRANCE);
+    public static SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
     public static SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm", Locale.FRANCE);
 
     public static boolean DateInRange(Date testStart , Date testEnd, Date actualStart, Date actualEnd) {
